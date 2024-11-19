056bfcb6da0ec7766b5fc31e8b3583c00a0b692e
mahsa <mkmi7171@gmail.com>
2024-11-18 20:06:08 -0500

    image slider in product detail page is done

--- src/components/ProjectsArray.jsx
+++ src/components/ProjectsArray.jsx
@@ -1,4 +1,25 @@
 import OstiganCard from "../assets/ostiganThumb.png";
+import Ostigan1 from '../assets/projectsPic/ostigan1.png'
+import Ostigan2 from '../assets/projectsPic/ostigan2.png'
+import Ostigan3 from '../assets/projectsPic/ostigan3.png'
+import Ostigan4 from '../assets/projectsPic/ostigan4.png'
+import Ostigan5 from '../assets/projectsPic/ostigan5.png'
+import Mehrayan1 from '../assets/projectsPic/mehrayan1.png'
+import Mehrayan2 from '../assets/projectsPic/mehrayan2.png'
+import Mehrayan3 from '../assets/projectsPic/mehrayan3.png'
+import Mehrayan4 from '../assets/projectsPic/mehrayan4.png'
+import Agro1 from '../assets/projectsPic/agro1.png'
+import Agro2 from '../assets/projectsPic/agro2.png'
+import Agro3 from '../assets/projectsPic/agro3.png'
+import Agro4 from '../assets/projectsPic/agro4.png'
+import Atc1 from '../assets/projectsPic/atc1.png'
+import Atc2 from '../assets/projectsPic/atc2.png'
+import Atc3 from '../assets/projectsPic/atc3.png'
+import Atc4 from '../assets/projectsPic/atc4.png'
+import One1 from '../assets/projectsPic/one1.png'
+import One2 from '../assets/projectsPic/one2.png'
+import One3 from '../assets/projectsPic/one3.png'
+import One4 from '../assets/projectsPic/one4.png'
 import MehrayanCard from '../assets/mehrayanThumb.png'
 import AtcCard from '../assets/atcThumb.png'
 import OneandintlCard from '../assets/oneandintlThumb.png'
@@ -6,10 +27,74 @@ import AgrotaminCard from '../assets/agroThumb.jpeg'
 import MahsaCard from '../assets/mahsa.jpeg'
 
 export const projects = [
-    { color: '#00BC8D', image: OstiganCard, title: "Ostigan", id:1 },
-    { color: '#F07646', image: AtcCard,  title: "ATC", id:2 },
-    { color: '#ECC77D', image: AgrotaminCard, title: "Agrotamin", id: 3 },
-    { color: '#F15B28', image: MehrayanCard,  title: "Mehrayan", id:4 },
-    { color: '#7D01BC', image: MahsaCard, title: "Portfolio", id:5 },
-    { color: '#7D01BC', image: OneandintlCard, title: "oneandintl", id:6 }
+    {
+        color: '#00BC8D',
+        image: OstiganCard,
+        title: "Ostigan",
+        id: 1,
+        detailedPics: [
+            { src: Ostigan5, alt: 'Ostigan feature 5' },
+            { src: Ostigan1, alt: 'Ostigan feature 1' },
+            { src: Ostigan2, alt: 'Ostigan feature 2' },
+            { src: Ostigan3, alt: 'Ostigan feature 3' },
+            { src: Ostigan4, alt: 'Ostigan feature 4' }
+        ]
+    },
+    {
+        color: '#F07646',
+        image: AtcCard,
+        title: "ATC",
+        id: 2,
+        detailedPics: [
+            { src: Atc1, alt: 'ATC feature 1' },
+            { src: Atc2, alt: 'ATC feature 2' },
+            { src: Atc3, alt: 'ATC feature 3' },
+            { src: Atc4, alt: 'ATC feature 4' },
+        ]
+    },
+    {
+        color: '#ECC77D',
+        image: AgrotaminCard,
+        title: "Agrotamin",
+        id: 3,
+        detailedPics: [
+            { src: Agro1, alt: 'Agrotamin feature 1' },
+            { src: Agro2, alt: 'Agrotamin feature 2' },
+            { src: Agro3, alt: 'Agrotamin feature 3' },
+            { src: Agro4, alt: 'Agrotamin feature 4' },
+        ]
+    },
+    {
+        color: '#F15B28',
+        image: MehrayanCard,
+        title: "Mehrayan",
+        id: 4,
+        detailedPics: [
+            { src: Mehrayan1, alt: 'Mehrayan feature 1' },
+            { src: Mehrayan2, alt: 'Mehrayan feature 2' },
+            { src: Mehrayan3, alt: 'Mehrayan feature 3' },
+            { src: Mehrayan4, alt: 'Mehrayan feature 4' },
+        ]
+    },
+    {
+        color: '#7D01BC',
+        image: MahsaCard,
+        title: "Portfolio",
+        id: 5,
+        detailedPics: [
+            { src: MahsaCard, alt: 'Portfolio feature 1' },
+        ]
+    },
+    {
+        color: '#7D01BC',
+        image: OneandintlCard,
+        title: "oneandintl",
+        id: 6,
+        detailedPics: [
+            { src: One1, alt: 'oneandintl feature 1' },
+            { src: One2, alt: 'oneandintl feature 2' },
+            { src: One3, alt: 'oneandintl feature 3' },
+            { src: One4, alt: 'oneandintl feature 4' },
+        ]
+    }
 ];
--- src/pages/ProjectDetail.jsx
+++ src/pages/ProjectDetail.jsx
@@ -1,67 +1,105 @@
 import { motion } from "framer-motion";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { projects } from "../components/ProjectsArray";
 
 const ProjectDetail = () => {
   const { id } = useParams();
   const project = projects.find((p) => p.id === parseInt(id));
+  const [animationDone, setAnimationDone] = useState(false);
 
   if (!project) return <div>Project not found</div>;
 
+  useEffect(() => {
+    // Set the state to true after the animation duration
+    const timer = setTimeout(() => setAnimationDone(true), 1000); // Adjust duration to match animation
+    return () => clearTimeout(timer);
+  }, []);
+
   const Arrow = ({ className }) => {
-    return <div className={`transition-all duration-500 ease-in-out ${className}`}>
-      <svg data-v-21a5ab71="" width="20" height="12" viewBox="0 0 18 17" fill="none" class="icon-svg | w-[1.8rem] h-auto"><path data-v-21a5ab71="" d="M4.75928 4.96442L13.3288 4.50811L12.5375 12.7426" class="stroke-current"></path><path data-v-21a5ab71="" d="M13.3857 4.39875L4.90039 12.884" class="stroke-current"></path></svg>
-    </div>
-  }
+    return (
+      <div className={`transition-all duration-500 ease-in-out ${className}`}>
+        <svg width="20" height="12" viewBox="0 0 18 17" fill="none" className="icon-svg | w-[1.8rem] h-auto">
+          <path d="M4.75928 4.96442L13.3288 4.50811L12.5375 12.7426" className="stroke-current"></path>
+          <path d="M13.3857 4.39875L4.90039 12.884" className="stroke-current"></path>
+        </svg>
+      </div>
+    );
+  };
 
   return (
-    <div className="h-screen bg-rose-300 flex items-center">
-      <div className="border">
+    <div className="relative h-screen bg-rose-300 overflow-hidden flex justify-between items-center">
+      {/* Left Section */}
+      <div className="flex-[0.85]">
         <h1 className="text-6xl font-safiro-reg-i number">{project.title}</h1>
-        <p className="w-1/2 py-8 tracking-tight leading-5">Improving lives and increasing
-          happiness through diverse STEM
-          talent leadership.</p>
-        <div className='uppercase overflow-hidden relative group border w-28 h-8 border-gray-800 px-2 py-1 rounded-xl'>
+        <p className="py-8 tracking-tight leading-5">
+          Improving lives and increasing happiness through diverse STEM talent leadership.
+        </p>
+        <div className="uppercase overflow-hidden relative group border w-28 h-8 border-gray-800 px-2 py-1 rounded-xl">
           <div className="flex items-center">
-            <p className='absolute top-2 text-sm group-hover:absolute group-hover:-top-[55%] group-hover:tracking-widest tracking-tighter transition-all duration-300 ease-in-out group-hover:opacity-0'>
+            <p className="absolute top-2 text-sm group-hover:absolute group-hover:-top-[55%] group-hover:tracking-widest tracking-tighter transition-all duration-300 ease-in-out group-hover:opacity-0">
               Visit
             </p>
-            <Arrow className={`absolute top-2 right-1 group-hover:-top-full group-hover:-right-4 group-hover:scale-50`} />
+            <Arrow
+              className={`absolute top-2 right-1 group-hover:-top-full group-hover:-right-4 group-hover:scale-50`}
+            />
           </div>
-          <p className='text-sm absolute -bottom-[50%] group-hover:bottom-1 transition-all tracking-widest group-hover:tracking-tighter duration-300 ease-in-out opacity-0 group-hover:opacity-100'>
+          <p className="text-sm absolute -bottom-[50%] group-hover:bottom-1 transition-all tracking-widest group-hover:tracking-tighter duration-300 ease-in-out opacity-0 group-hover:opacity-100">
             visit
           </p>
-          <Arrow className={'absolute top-full right-4 scale-50 group-hover:top-2 group-hover:right-1 group-hover:scale-100'} />
+          <Arrow
+            className={
+              "absolute top-full right-4 scale-50 group-hover:top-2 group-hover:right-1 group-hover:scale-100"
+            }
+          />
         </div>
       </div>
-      <div className="slider">
+
+      {/* Animated Image Section */}
+      <div
+        className={`slider h-screen p-8 w-[40%] flex-[1.3] ${animationDone ? "overflow-y-scroll" : ""
+          }`}
+      >
+        {/* Motion Animated Image */}
         <motion.div
-          className="flex flex-col items-center justify-center h-screen"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.5 }}
+          className="flex items-center justify-center w-[580px] h-[320px]"
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1 }} // Match timeout in useEffect
         >
           <motion.div
-            layoutId={`project-image-${project.id}`} 
-            className="relative w-[500px] h-[300px] rounded-lg overflow-hidden"
-            initial={{ borderRadius: "50%" }} 
+            layoutId={`project-image-${project.id}`}
+            className="relative w-[580px] h-[320px] rounded-lg overflow-hidden"
+            initial={{ borderRadius: "50%" }}
             animate={{ borderRadius: "16px" }}
-            transition={{ duration: 0.8 }}
+            transition={{ duration: 1 }}
           >
             <img
-              src={project.image}
+              src={project.detailedPics[0].src}
               alt={project.title}
               className="absolute inset-0 w-full h-full object-cover"
             />
           </motion.div>
         </motion.div>
+
+        {/* Mapped Images - Render After Animation */}
+        {animationDone &&
+          project.detailedPics.slice(1).map((image, index) => (
+            <img
+              key={index}
+              src={image.src}
+              alt={image.alt}
+              className="w-[580px] h-[320px] object-cover object-center rounded-2xl mt-8"
+            />
+          ))}
       </div>
-      <div className="px-4">
-        <div className="flex">
-          <span className=" text-6xl tracking-tight number">0{project.id}</span>
+
+      {/* Right Section */}
+      <div className="flex-[0.85]">
+        <div className="flex items-center">
+          <span className="text-6xl tracking-tight number">0{project.id}</span>
           <div className="w-0.5 mx-8 h-16 bg-gray-400 rotate-45"></div>
-          <span className="text-6xl tracking-tight  number">06</span>
+          <span className="text-6xl tracking-tight number">06</span>
         </div>
         <div>
           <div>
@@ -70,7 +108,7 @@ const ProjectDetail = () => {
           </div>
           <div>
             <p className="number font-bold font-safiro-reg-i">Technologies</p>
-            <span className="text-sm">React, Next , Typescript</span>
+            <span className="text-sm">React, Next, TypeScript</span>
           </div>
           <div>
             <p className="number font-bold font-safiro-reg-i">Year</p>
